6091845672678109991ea1267791226abdc1120e
leonardobenedeti <leonardobenedeti@gmail.com>
2019-02-18 10:10:14 -0300

    Ajustes de banco para rodar no heroku

--- api.py
+++ api.py
@@ -1,15 +1,15 @@
 from flask import Flask, jsonify
-from flask.ext.mysql import MySQL
+from flask.ext.mysql import MySQL 
 
 app = Flask(__name__)
 
 mysql = MySQL()
 
 # MySQL configurations
-app.config['MYSQL_DATABASE_USER'] = 'USER'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'PASS'
-app.config['MYSQL_DATABASE_DB'] = 'DB'
-app.config['MYSQL_DATABASE_HOST'] = 'HOST'
+app.config['MYSQL_DATABASE_USER'] = 'b45fc1daa17d12'
+app.config['MYSQL_DATABASE_PASSWORD'] = '910f54a1'
+app.config['MYSQL_DATABASE_DB'] = 'heroku_428b368c435e5d2'
+app.config['MYSQL_DATABASE_HOST'] = 'us-cdbr-iron-east-03.cleardb.net'
 mysql.init_app(app)
 
 
@@ -18,11 +18,11 @@ mysql.init_app(app)
 def index():
     conn = mysql.connect()
     cursor = conn.cursor()
-    signin = cursor.execute("SELECT * FROM TABLE")
-    signin = cursor.fetchone()
+    users = cursor.execute("SELECT * FROM users")
+    users = cursor.fetchall()
 
     conn.close()
-    return jsonify(signin)
+    return jsonify(users)
 
 
 if __name__ == "__main__":
